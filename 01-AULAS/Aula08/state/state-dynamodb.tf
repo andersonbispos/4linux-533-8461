d7a0fa5bbd9710c7f76492c6681dea334b7f9e33
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-14 11:35:48 -0300

    dynamodb

---
+++ 01-AULAS/Aula08/state/state-dynamodb.tf
@@ -0,0 +1,15 @@
+resource "aws_dynamodb_table" "dynamodb_table_state" {
+  name           = "terraform-lock"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name        = "dynamodb_table_state"
+  }
+}

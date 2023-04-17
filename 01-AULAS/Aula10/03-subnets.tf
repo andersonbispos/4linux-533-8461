1da37156d39627a3139eb1bf3b96f133e64393f3
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-17 12:37:43 -0300

    subnets

--- 01-AULAS/Aula10/03-subnets.tf
+++ 01-AULAS/Aula10/03-subnets.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "subnet0" {
   vpc_id = aws_vpc.vpc_terraform.id
 
   //usando variaveis individuais
@@ -10,22 +10,29 @@ resource "aws_subnet" "subnet1" {
   cidr_block = var.subnet_faixas[0]
 
   tags = {
-    Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet1_zona)
+    //Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet1_zona)
+    Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet_zones[0])
+  }
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vpc_terraform.id
+
+  availability_zone = var.subnet_zones[1]
+  cidr_block = var.subnet_faixas[1]
+
+  tags = {
+    Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet_zones[1])
   }
 }
 
 resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.vpc_terraform.id
 
-  //usando variaveis individuais
-  //availability_zone = var.subnet2_zona
-  // cidr_block = var.subnet2_cidr
-
-  //usando lista
-  availability_zone = var.subnet_zones[1]
-  cidr_block = var.subnet_faixas[1]
+  availability_zone = var.subnet_zones[2]
+  cidr_block = var.subnet_faixas[2]
 
   tags = {
-    Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet2_zona)
+    Name = format("%s-%s-%s", var.subnet_prefix_name, var.vpc_name, var.subnet_zones[2])
   }
 }

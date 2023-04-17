7b44ca7a393a07e1cd087c69b196ae37937090ef
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-17 10:50:04 -0300

    change .gitignore to permit .tfvars

--- 01-AULAS/Aula10/03-subnets.tf
+++ 01-AULAS/Aula10/03-subnets.tf
@@ -1,15 +1,15 @@
-resource "aws_subnet" "subnet_terraform_east_1b" {
+resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.vpc_terraform.id
   cidr_block = var.subnet1_cidr
 
   availability_zone = var.subnet1_zona
 
   tags = {
-    Name = "subnet_terraform_east_1b"
+    Name = format("%s-%s-%s", "subnet_", var.vpc_name, var.subnet1_zona )
   }
 }
 
-resource "aws_subnet" "subnet_terraform_east_1c" {
+resource "aws_subnet" "subnet2" {
   vpc_id     = aws_vpc.vpc_terraform.id
   cidr_block = var.subnet2_cidr
 
---
+++ 01-AULAS/Aula10/terraform.tfvars
@@ -0,0 +1,2 @@
+//vpc_name = "vcp_aula10"
+//vpc_cidr = "192.168.10.0/24"

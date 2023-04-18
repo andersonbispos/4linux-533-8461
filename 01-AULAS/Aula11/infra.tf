4e68067af1597bac949e5526606c4066ea069f86
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 11:15:22 -0300

    infra

--- 01-AULAS/Aula11/infra.tf
+++ 01-AULAS/Aula11/infra.tf
@@ -1,12 +1,12 @@
 module "rede" {
-  source = "./modulos/vpc"
+  source = "git::https://github.com/andersonbispos/4linux-533-8461//01-AULAS/Aula11/modulos/vpc"
 
   vpc_cidr = "192.168.10.0/24"
 }
 
-resource "aws_subnet" "subnet1" {
+/* resource "aws_subnet" "subnet1" {
   vpc_id     = module.rede.vpc_id
   cidr_block = "192.168.10.0/25"
 
   availability_zone = "us-east-1c"
-}
+} */

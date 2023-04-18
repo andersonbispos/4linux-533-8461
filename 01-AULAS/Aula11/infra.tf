50b7ae68233a58d2185d6def5180a76c9c16cbc7
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 11:13:55 -0300

    aula11 extra

--- 01-AULAS/Aula11/infra.tf
+++ 01-AULAS/Aula11/infra.tf
@@ -10,16 +10,3 @@ resource "aws_subnet" "subnet1" {
 
   availability_zone = "us-east-1c"
 }
-
-resource "aws_instance" "vm1" {
-  ami           = "ami-06e46074ae430fba6" # <- amazonl linux us-east1
-  instance_type = "t3a.micro"
-
-  subnet_id = aws_subnet.subnet1.id
-
-  vpc_security_group_ids = [module.rede.sg_id]
-
-  tags = {
-    Name = "vm1"
-  }
-}

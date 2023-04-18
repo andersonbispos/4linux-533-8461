ccb3e320083782067c05a0e540571c16e6b4e4fd
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 11:04:33 -0300

    exemplos modulos

--- 01-AULAS/Aula11/infra.tf
+++ 01-AULAS/Aula11/infra.tf
@@ -3,3 +3,23 @@ module "rede" {
 
   vpc_cidr = "192.168.10.0/24"
 }
+
+resource "aws_subnet" "subnet1" {
+  vpc_id     = module.rede.vpc_id
+  cidr_block = "192.168.10.0/25"
+
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_instance" "vm1" {
+  ami               = "ami-06e46074ae430fba6" # <- amazonl linux us-east1
+  instance_type     = "t2.micro"
+
+  subnet_id = aws_subnet.subnet1.id
+
+  vpc_security_group_ids = [ module.rede.sg_id ]
+
+  tags = {
+    Name = "vm1"
+  }
+}

e58485bac3aad71542abe3a9420d7ca8001ef25b
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 11:18:34 -0300

    modulos

--- 01-AULAS/Aula11-infraexterna/infra.tf
+++ 01-AULAS/Aula11-infraexterna/infra.tf
@@ -1,25 +1,12 @@
 module "rede" {
-  source = "./modulos/vpc"
+  source = "github.com/andersonbispos/4linux-533-8461//01-AULAS/Aula11/modulos/vpc"
 
   vpc_cidr = "192.168.10.0/24"
 }
 
-resource "aws_subnet" "subnet1" {
+/* resource "aws_subnet" "subnet1" {
   vpc_id     = module.rede.vpc_id
   cidr_block = "192.168.10.0/25"
 
   availability_zone = "us-east-1c"
-}
-
-resource "aws_instance" "vm1" {
-  ami           = "ami-06e46074ae430fba6" # <- amazonl linux us-east1
-  instance_type = "t3a.micro"
-
-  subnet_id = aws_subnet.subnet1.id
-
-  vpc_security_group_ids = [module.rede.sg_id]
-
-  tags = {
-    Name = "vm1"
-  }
-}
+} */

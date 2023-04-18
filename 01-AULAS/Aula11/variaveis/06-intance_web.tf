87cf017a1b6826bd520530052a9bb63bd6ec2955
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 09:33:26 -0300

    instance

---
+++ 01-AULAS/Aula11/variaveis/06-intance_web.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "web1" {
+  ami           = var.ami_amazon
+  instance_type = var.web_size
+
+  subnet_id                   = aws_subnet.subnets[0].id
+  associate_public_ip_address = var.associate_public_ip
+
+  //vpc_security_group_ids = [aws_security_group.sg_vpc_terraform.id]
+
+  tags = {
+    Name = format("%s-%s", var.web_prefix_name, var.vpc_name)
+  }
+
+  user_data = base64encode("#!/bin/bash\n sudo yum install nginx -y; sudo systemctl enable nginx; sudo systemctl start nginx")
+}

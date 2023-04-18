05393d95638109d762db76bd38ba5538ebdc9ee4
Anderson Bispo <anderson.bispo@4linux.com.br>
2023-04-18 11:54:12 -0300

    modules

--- 01-AULAS/Aula11-infraexterna/infra.tf
+++ 01-AULAS/Aula11-infraexterna/infra.tf
@@ -4,9 +4,17 @@ module "rede" {
   vpc_cidr = "192.168.10.0/24"
 }
 
-/* resource "aws_subnet" "subnet1" {
+resource "aws_subnet" "subnet1" {
   vpc_id     = module.rede.vpc_id
   cidr_block = "192.168.10.0/25"
 
   availability_zone = "us-east-1c"
-} */
+}
+
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "4.3.0"
+
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [ module.rede.sg_id ]
+}
